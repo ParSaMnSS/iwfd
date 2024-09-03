1cf8c18a0103ead9327bf483898d28a76769fcd6
MNS <parsamanes@yahoo.com>
2024-09-03 11:12:04 +0300

    i give up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#1692AC',
+        'custom-pink': '#AA1A55',
+        'custom-red': '#F7435F',
+        'custom-orange': '#F78160',
+      },
+    },
   },
   plugins: [],
 }
